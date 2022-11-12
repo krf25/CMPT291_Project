f83d8813d57b99b34c189463a9ef4ff7eddc068d
Tay-Carr <taylor._.c@hotmail.com>
2022-11-12 12:03:42 -0700

    Employee report tabs

---
+++ CMPT291_Project/EmpReports.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CMPT291_Project
+{
+    public partial class EmpReports : Form
+    {
+        public EmpReports()
+        {
+            InitializeComponent();
+        }
+    }
+}
