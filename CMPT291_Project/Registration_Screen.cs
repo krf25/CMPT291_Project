102e5ff939ea54d0528c01224c6b192851e93d28
Luusshh <108303939+Luusshh@users.noreply.github.com>
2022-11-24 17:58:30 -0700

    should function as a registration form should
    checks for failed confirm password field, if the account being registered has already been registered/already exists, then creates the new customer login info if it does not already exist in the customer table.

--- CMPT291_Project/Registration_Screen.cs
+++ CMPT291_Project/Registration_Screen.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using Microsoft.VisualBasic.ApplicationServices;
+using CMPT291_Project;
 
 namespace CMPT291_Group3_Project
 {
@@ -17,17 +19,75 @@ namespace CMPT291_Group3_Project
         {
             InitializeComponent();
         }
-
+        // connection to database
+        SqlConnection db = new SqlConnection(@"Data Source=(local);Initial Catalog=Cmpt291_GroupProject;Integrated Security=True");
         private void label1_Click(object sender, EventArgs e)
         {
 
         }
 
         private void Register_button_Click(object sender, EventArgs e)
-        {
-            //if (password_box.Text != String.Empty || password_box_confirm.Text != String.Empty || username_box.Text != String.Empty)
-            //{ }
+        {   // check for empty text boxes
+            if (password.Text != String.Empty || password_confirm.Text != String.Empty || email.Text != String.Empty)
+            {   // wrong confirm password
+                if (password.Text != password_confirm.Text)
+                {
+                    MessageBox.Show("Password wasn't confirmed correctly.", "Confirm password failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    password.Clear();
+                    password_confirm.Clear();
+                    password.Focus();
+                    return;
+                }
+                // query the database with SqlDataAdapter, add query response to table obj
+                string query = "Select * FROM customer WHERE Email ='" + email.Text + "'";// AND password = '" + password.Text + "'";
+                SqlDataAdapter adapter = new(query, db);
+                DataTable emp_emails = new DataTable();
+                adapter.Fill(emp_emails);
+                
+                if (emp_emails.Rows.Count > 0)
+                {
+                    MessageBox.Show("Account already exists.", "Account already exists", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    password.Clear();
+                    password_confirm.Clear();
+                    email.Focus();
+                    return;
+                }
+
+
+                else if (emp_emails.Rows.Count == 0 && password.Text == password_confirm.Text)
+                {
+                    // generate unique id -> get max ID from table, convert to int, increment by one then convert back to string
+                    db.Open();
+                    int max_value = 0;
+                    string LID_query = "Select max(CID) FROM Customer";
+                    SqlCommand cmd = new SqlCommand(LID_query, db);
+                    max_value = (int)cmd.ExecuteScalar(); // executescalar returns first col/row item from queried table
+                    max_value++;
+
+                    // inserting the registration into the customer table
+                    // TID 0 - no plan; (1-4) limited, basic, standard, premium
+                    string email_text = email.Text;
+                    string password_text = password.Text;
+                    string r = "Insert into Customer values('" + max_value + "'" +
+                        ", '', '', '', '', '', '', '', '" + email_text + "'," +
+                        "'" + password_text + "', '', 1, 1, '', '')";
+
+                    // executeNonQuery - "Executes a Transact-SQL statement against the connection and returns the number of rows affected."
+                    SqlCommand insertion = new SqlCommand(r, db);
+                    insertion.ExecuteNonQuery();
+                    MessageBox.Show("Account Registered.", "Registration Complete", MessageBoxButtons.OK);
+                    this.Hide();
+                    db.Close();
+
+                }
+            
+            }
+            else
+            {
+                MessageBox.Show("Registration error, please fill in the required fields correctly.", "Registration Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
+        
 
         private void username_box_TextChanged(object sender, EventArgs e)
         {
@@ -37,7 +97,7 @@ namespace CMPT291_Group3_Project
         private void Login_redirect_button_Click(object sender, EventArgs e)
         {
             // Check Login form is still open, close registration form
-            // DO NOT close login form before/after registration form operations
+            // DO NOT close login form before/after registration form opens
             FormCollection forms = Application.OpenForms;
             foreach (Form f in forms)
             {
@@ -47,6 +107,16 @@ namespace CMPT291_Group3_Project
                     return;
                 }
             }
+        }
+
+        private void bkround_panel_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Registration_Screen_Load(object sender, EventArgs e)
+        {
+            // form opens to username being focused instead of email despite email being at the top; email.Focus()
         }
     }
 }
