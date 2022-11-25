1f471ef1b17ba62bf18f7e5891e4bd9e0a84c318
Tay-Carr <taylor._.c@hotmail.com>
2022-11-24 21:45:17 -0700

    connected the links from landing page, fixed db connection

--- CMPT291_Project/landingpage.Designer.cs
+++ CMPT291_Project/landingpage.Designer.cs
@@ -40,10 +40,9 @@
             // 
             this.welcome_message.AutoSize = true;
             this.welcome_message.Font = new System.Drawing.Font("Segoe UI", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.welcome_message.Location = new System.Drawing.Point(18, 19);
-            this.welcome_message.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.welcome_message.Location = new System.Drawing.Point(51, 61);
             this.welcome_message.Name = "welcome_message";
-            this.welcome_message.Size = new System.Drawing.Size(467, 54);
+            this.welcome_message.Size = new System.Drawing.Size(1404, 159);
             this.welcome_message.TabIndex = 0;
             this.welcome_message.Text = "Welcome back, Manager!";
             // 
@@ -51,9 +50,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(54, 94);
+            this.label1.Location = new System.Drawing.Point(154, 301);
+            this.label1.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(78, 21);
+            this.label1.Size = new System.Drawing.Size(232, 65);
             this.label1.TabIndex = 0;
             this.label1.Text = "Customer";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -62,9 +62,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(54, 118);
+            this.label2.Location = new System.Drawing.Point(154, 378);
+            this.label2.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 21);
+            this.label2.Size = new System.Drawing.Size(159, 65);
             this.label2.TabIndex = 1;
             this.label2.Text = "Movie";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -73,9 +74,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(54, 169);
+            this.label3.Location = new System.Drawing.Point(154, 554);
+            this.label3.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 21);
+            this.label3.Size = new System.Drawing.Size(190, 65);
             this.label3.TabIndex = 2;
             this.label3.Text = "Reports";
             this.label3.Click += new System.EventHandler(this.label3_Click);
@@ -84,9 +86,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.label4.Location = new System.Drawing.Point(54, 145);
+            this.label4.Location = new System.Drawing.Point(154, 464);
+            this.label4.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(54, 21);
+            this.label4.Size = new System.Drawing.Size(160, 65);
             this.label4.TabIndex = 3;
             this.label4.Text = "Rental";
             this.label4.Click += new System.EventHandler(this.label4_Click);
@@ -95,27 +98,27 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
-            this.label5.Location = new System.Drawing.Point(54, 199);
+            this.label5.Location = new System.Drawing.Point(154, 637);
+            this.label5.Margin = new System.Windows.Forms.Padding(9, 0, 9, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(85, 21);
+            this.label5.Size = new System.Drawing.Size(254, 65);
             this.label5.TabIndex = 4;
             this.label5.Text = "Employees";
             this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
-            // EmpLandingpage
+            // landingpage
             // 
             this.AccessibleName = "landing_page";
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(686, 396);
+            this.ClientSize = new System.Drawing.Size(1960, 1267);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.welcome_message);
-            this.Margin = new System.Windows.Forms.Padding(1);
-            this.Name = "EmpLandingpage";
+            this.Name = "landingpage";
             this.Text = "Welcome Employee";
             this.ResumeLayout(false);
             this.PerformLayout();
